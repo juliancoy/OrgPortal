077dc9a78ebff5d3066bf1de7913053f6a9909bb
Julian Coy <jcloiacon@gmail.com>
2026-06-07 22:28:40 -0400

    Add typo-tolerant network search

--- web/src/ui/shell/Footer.tsx
+++ web/src/ui/shell/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
       <div className="portal-footer-inner">
         <div>
           <div className="portal-brand-title">Code Collective</div>
-          <div className="portal-brand-sub">Civic governance, transparently.</div>
+          <div className="portal-brand-sub">Coding a new economy.</div>
         </div>
         <span>© 2026 Code Collective</span>
       </div>
